bdbedf46635a32766c260f132fd59eb9929ebd09
Selin <selin@Selin-MacBook-Air.local>
2023-05-23 02:21:28 +0300

    Added new CLI

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,9 @@ def wiki(name="War Goddess", lenght=1):
 
     my_wiki = wikipedia.summary(name, lenght)
     return my_wiki
+
+
+def search_wiki(name):
+    """Search wikipedia for names"""
+    results = wikipedia.search(name)
+    return results

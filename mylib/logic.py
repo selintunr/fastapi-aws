8068f8c5670f8ddd3715a0bf613c60ef08ecdae8
Selin <selin@Selin-MacBook-Air.local>
2023-05-23 02:09:11 +0300

    Added GLI

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,7 +1,8 @@
 import wikipedia
 
-def wiki(name="War Goddess",lenght=1):
+
+def wiki(name="War Goddess", lenght=1):
     """This is a wikipedia fetcher"""
 
-    my_wiki= wikipedia.summary(name,lenght)
+    my_wiki = wikipedia.summary(name, lenght)
     return my_wiki
